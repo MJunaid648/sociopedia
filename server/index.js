12c482032d39db80c10ae90dd03b85acb86ca664
Muhammad Junaid <648mjunaid@gmail.com>
2023-06-27 15:30:45 +0500

    creating schemas and models as well as middlewares and controllers

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { verifyToken } from "./middlewares/auth.js";
+import { createPost } from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
 
 //  COFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -35,3 +41,21 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+//ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture", createPost));
+
+// ROUTES
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
+//Mongoose setup
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(app.listen(PORT, () => console.log(`Server PORT: ${PORT}`)))
+  .catch((err) => console.log(`${err} did not connect`));
