3e82b0295ef6cc3cba58d7b41182d82e0ec3a7ef
Muhammad Junaid <muhammad.junaid@adroittech.eu>
2024-01-24 16:29:02 +0500

    created server register and login routes

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from './routes/auth.js'
 import {register} from './controllers/auth.js'
 // CONFIGURATIONS
 const __fileName = fileURLToPath(import.meta.url);
